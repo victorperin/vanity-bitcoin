ec83ac7e545dd6b1922be88e1ab470c1e5bada9e
Victor Perin <victor.perin@ingaia.com.br>
2017-09-06 11:29:28 -0300

    style(index): moving vanitygen to clean code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const { spawn } = require('child_process');
 const filter = require('through2-filter');
 
-const vanitygen = spawn('bin/vanitygen', ['-i', '-k', '1Perin']);
-
 const processBuffer = require('./process-buffer');
 
 const onlyKeys = filter( (chunk) => chunk.toString().includes('Key') );
 
+const vanitygen = spawn('bin/vanitygen', ['-i', '-k', '1Perin']);
 vanitygen.stdout
   .pipe(onlyKeys)
   .on('data', processBuffer);

8707a4140123c99c878949d085d3441001c94923
Victor Perin <victor.perin@ingaia.com.br>
2017-09-06 11:08:54 -0300

    refactor(index): segregating process-buffer

--- src/index.js
+++ src/index.js
@@ -3,19 +3,13 @@ const filter = require('through2-filter');
 
 const vanitygen = spawn('bin/vanitygen', ['-i', '-k', '1Perin']);
 
-const processAddress = (buffer) =>
-  Promise.resolve(buffer)
-    .then( (buffer) => buffer.toString() )
-    .then( text => text.match(/Private Key\:\s+(\w+)\s+Address:\s+(\w+)/i) )
-    .then( ([string, privateKey, publicKey]) => ({ privateKey, publicKey }) )
-    .then(console.log)
-
+const processBuffer = require('./process-buffer');
 
 const onlyKeys = filter( (chunk) => chunk.toString().includes('Key') );
 
 vanitygen.stdout
   .pipe(onlyKeys)
-  .on('data', processAddress);
+  .on('data', processBuffer);
 
 vanitygen.stderr.on('data', console.error);
 

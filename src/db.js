66a093c0a5da2d7e6b6725d2bb123ea3a3bdbfe8
Victor Perin <victor.perin@ingaia.com.br>
2017-09-11 13:49:08 -0300

    init(db)

---
+++ src/db.js
@@ -0,0 +1,21 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ adresses: [] })
+  .write();
+
+const saveKey = (key) => {
+  const teste = db.get('adresses')
+    .push(key)
+    .write();
+
+  console.log(teste);
+  return teste;
+}
+
+module.exports = {
+  saveKey,
+};

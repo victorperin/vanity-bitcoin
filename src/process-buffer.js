909468b2c82aa714dc699aae553f17b82047d41c
Victor Perin <victor.perin@ingaia.com.br>
2017-09-11 13:59:37 -0300

    feat(process-buffer): save to db

--- src/process-buffer.js
+++ src/process-buffer.js
@@ -1,6 +1,8 @@
+const db = require('./db');
+
 module.exports = (buffer) =>
   Promise.resolve(buffer)
     .then( (buffer) => buffer.toString() )
     .then( text => text.match(/Private Key\:\s+(\w+)\s+Address:\s+(\w+)/i) )
     .then( ([string, privateKey, publicKey]) => ({ privateKey, publicKey }) )
-    .then(console.log)
+    .then(db.saveKey)
